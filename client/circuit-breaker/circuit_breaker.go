e040294426417c12019e2e01ecf30c7e78273626
Roman Kazarov <roman.kazarov@gett.com>
2023-04-01 21:26:21 +0200

    Add default action

--- client/circuit-breaker/circuit_breaker.go
+++ client/circuit-breaker/circuit_breaker.go
@@ -14,27 +14,29 @@ const (
 )
 
 type CircuitBreaker struct {
-	State        CircuitBreakerState
-	FailureCount int
-	Threshold    int
-	Timeout      time.Duration
-	LastFailure  time.Time
+	State         CircuitBreakerState
+	FailureCount  int
+	Threshold     int
+	Timeout       time.Duration
+	LastFailure   time.Time
+	DefaultAction func() (string, error)
 }
 
-func NewCircuitBreaker(threshold int, timeout time.Duration) *CircuitBreaker {
+func NewCircuitBreaker(threshold int, timeout time.Duration, defaultAction func() (string, error)) *CircuitBreaker {
 	return &CircuitBreaker{
-		State:        StateClosed,
-		FailureCount: 0,
-		Threshold:    threshold,
-		Timeout:      timeout,
-		LastFailure:  time.Now(),
+		State:         StateClosed,
+		FailureCount:  0,
+		Threshold:     threshold,
+		Timeout:       timeout,
+		LastFailure:   time.Now(),
+		DefaultAction: defaultAction,
 	}
 }
 
-func (cb *CircuitBreaker) Call(protectedFunc func() (string, error)) (string, error) {
+func (cb *CircuitBreaker) Call(action func() (string, error)) (string, error) {
 	switch cb.State {
 	case StateClosed:
-		success, err := protectedFunc()
+		success, err := action()
 		if err != nil {
 			cb.FailureCount++
 			cb.LastFailure = time.Now()
@@ -49,19 +51,20 @@ func (cb *CircuitBreaker) Call(protectedFunc func() (string, error)) (string, er
 	case StateOpen:
 		if time.Since(cb.LastFailure) >= cb.Timeout {
 			cb.State = StateHalfOpen
-			return cb.Call(protectedFunc)
+			return cb.Call(action)
 		}
-		return "", errors.New("circuit breaker is open")
+		return cb.DefaultAction()
 
 	case StateHalfOpen:
-		success, err := protectedFunc()
+		success, err := action()
 		if err != nil {
 			cb.State = StateOpen
 			cb.LastFailure = time.Now()
-		} else {
-			cb.State = StateClosed
-			cb.FailureCount = 0
+			return cb.DefaultAction()
 		}
+		cb.State = StateClosed
+		cb.FailureCount = 0
+
 		return success, err
 	}
 
--- client/main.go
+++ client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	circuitBreaker "github.com/KRR19/CircuitBreaker/client/circuit-breaker"
 	"net/http"
@@ -9,16 +10,20 @@ import (
 
 func sendRequest() (string, error) {
 	res, err := http.Head("http://localhost:8081/hello")
-	if err != nil || (res.StatusCode < 200 && res.StatusCode > 299) {
-		return "", err
+	if err != nil || (res.StatusCode < 200 || res.StatusCode > 299) {
+		return "", errors.New("server failed")
 	}
 
 	return res.Header.Get("Result"), nil
 }
 
+func defaultAction() (string, error) {
+	return "DEFAULT", nil
+}
+
 func main() {
-	cb := circuitBreaker.NewCircuitBreaker(5, 2*time.Second)
-	for i := 0; i < 100000; i++ {
+	cb := circuitBreaker.NewCircuitBreaker(5, 3*time.Second, defaultAction)
+	for i := 0; i < 10000000; i++ {
 		result, err := cb.Call(sendRequest)
 		if err != nil {
 			fmt.Println("Error")
